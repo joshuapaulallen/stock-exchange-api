519fbb269a48a736989325372587540b3137e994
josh <joshuapaulallen@gmail.com>
2019-04-13 10:40:29 -0600

    initial commit: an empty spring boot app

---
+++ src/main/java/org/yourotherleft/stockexchange/StockExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package org.yourotherleft.stockexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockExchangeApiApplication.class, args);
+    }
+
+}
